369e8c232970b37c0ac5791dac254e926b41f21c
Jose Lopez <jjlz.cocoa@gmail.com>
2018-07-24 17:31:03 -0500

    Button Reset and connections with storyboard added

--- RSP_Game/Controllers/ViewController.swift
+++ RSP_Game/Controllers/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ivUserSelection: UIStackView!
+    @IBOutlet weak var ivComputerSelection: UIImageView!
+    @IBOutlet weak var lblUserScore: UILabel!
+    @IBOutlet weak var lblComputerScore: UILabel!
+    @IBOutlet weak var lblResultMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnSelectionTapped(_ sender: Any) {
+        
+//        let button = sender as! UIButton
+    }
+    
+    @IBAction func btnResetTapped(_ sender: Any) {
+        
+    }
 }
 
